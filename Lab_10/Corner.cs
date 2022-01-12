47502f7d1fa3f641d5c8cd0c8be01ebb4caf3c5c
stasserebrennikov59@gmail.com <stasserebrennikov59@gmail.com>
2022-01-13 00:46:46 +0300

    Добавьте файлы проекта.

---
+++ Lab_10/Corner.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    internal class Corner
+    {
+        int gradus;
+        int minute;
+        int sec;
+        public bool isCorrect = true;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value < 0 || value > 359)
+                    isCorrect = false;
+                else
+                    gradus = value;
+            }
+
+        }
+        public int Minute
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    Gradus += value / 60;
+                    minute = value % 60;
+                }
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    Minute += value / 60;
+                    sec = value % 60;
+                }
+            }
+        }
+        public Corner(int gradus, int minute, int second)
+        {
+            this.Gradus = gradus;
+            this.Minute = minute;
+            this.Sec = second;
+        }
+        public double ToRadians()
+        {
+            return gradus * Math.PI / 180;
+        }
+    }
+}
---
+++ Lab_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Corner corner;
+            do
+            {
+                Console.WriteLine("Введите данные");
+                int gradus_ = Convert.ToInt32(Console.ReadLine());
+                int minute_ = Convert.ToInt32(Console.ReadLine());
+                int sec_ = Convert.ToInt32(Console.ReadLine());
+                corner = new Corner(gradus_, minute_, sec_);
+            }
+            while (corner.isCorrect == false);
+            double totalCorner = corner.ToRadians();
+            Console.WriteLine(totalCorner);
+            Console.ReadKey();
+        }
+    }
+}
